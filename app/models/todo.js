dad49540b53bc208de12b8443a8d11b811080a14
Lee Sinclair <lee.j.sinclair@gmail.com>
2015-11-30 11:42:10 +1000

    Ensure DB models support validation / required values and unique combinations for todo text

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,8 +1,19 @@
 var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
 
-module.exports = mongoose.model('Todo', {
-  text: {
-    type: String,
-    default: ''
-  }
+var Todo = new Schema({
+  'text': {
+    'type': String,
+    'required': 'Please enter a Todo'
+  },
+  'done': { 'type': Number, 'default': -1 } /* -1 = not done */
 });
+
+// provide the ability sort items at speed
+Todo.index({ 'text': 1 });
+
+// index by, text and done. ensure todo itemas are unique
+// this means that people can add todo items again as new items if an existing item has already been completed
+Todo.index({ 'text': 1, 'done': 1 }, { 'unique': true });
+
+module.exports = mongoose.model('Todo', Todo );
