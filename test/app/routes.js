b9e2f55fd35386c67a3adc2f058c6f3fe2c9b8e9
Lee Sinclair <lee.j.sinclair@gmail.com>
2015-11-30 14:48:45 +1000

    API tests

--- test/app/routes.js
+++ test/app/routes.js
@@ -5,35 +5,129 @@ var expect = require('expect.js');
 var rewire = require('rewire');
 var request = require('supertest');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+var mongoose = require('mongoose');
+var dbURI = 'mongodb://localhost:27017/test_todos';
 
 // The routes module attaches Express API routes to the app
 var routes = rewire('../../app/routes.js');
 routes(app);
 
 describe('app/routes.js tests', function() {
+  /*
   before(function mockCallsToMongoDb() {
     routes.__set__('Todo', {
-      find: function find(conditions, projection, options, callback) {
+      'find': function find(conditions, projection, options, callback) {
         var payload = [{ text: 'Todo 1' }, { text: 'Todo 2' }, { text: 'Todo 3' }, { text: 'Todo 4' }, { text: 'Todo 5' }, { text: 'Todo 6' }];
         conditions(null, payload);
       }
     });
   });
+  */
+ 
+  before(function(done) {
+    mongoose.connect( dbURI ); // Connect to test DB
 
-  it('returns expected payload for /api/todos route', function(done) {
+    done();
+  });
+
+  // Create brand new item
+  it('create new todo item /api/todos route', function(done) {
     request(app)
-      .get('/api/todos')
+      .post('/api/todos')
+      .send( { 'text': 'Todo 1'} )
       .expect('Content-Type', /json/)
-      .expect(200)
+      .expect(201)
       .end(function(err, res) {
-        expect(res.body.length).to.eql(6);
+        expect(res.body.length).to.eql(1);
         expect(res.body[0].text).to.eql('Todo 1');
 
         done();
       });
   });
 
+  // Attempt to add the same item again
+  it('create new todo item, test duplicate error /api/todos route', function(done) {
+    request(app)
+      .post('/api/todos')
+      .send( {  'text': 'Todo 1'} )
+      .expect('Content-Type', /json/)
+      .expect(409)
+      .end(function(err, res) {
+        expect(res.body.message).to.contain('Todo 1');
+
+        done();
+      });
+  });
+
+  // Attempt to add an item with no text
+  it('create new todo item, test null entry /api/todos route', function(done) {
+    request(app)
+      .post('/api/todos')
+      .send( { 'text': null} )
+      .expect('Content-Type', /json/)
+      .expect(422)
+      .end(function(err, res) {
+        expect(res.body.message).to.contain('Please enter a Todo');
+
+        done();
+      });
+  });
+
+  // List all todo items
+  it('returns expected payload for /api/todos route', function(done) {
+    request(app)
+      .get('/api/todos')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        expect(res.body.length).to.eql(1);
+        expect(res.body[0].text).to.eql('Todo 1');
+
+        done();
+      });
+  });
+
+  // Attempt to delete an item with an invalid ID
+  it('delete item, invalid item /api/todos/:id route', function(done) {
+    request(app)
+      .delete('/api/todos/1234')
+      .expect('Content-Type', /json/)
+      .expect(500)
+      .end(function(err, res) {
+
+        done();
+      });
+  });
+
+  // Delete an item with a valid ID
+  it('delete item, valid item /api/todos/:id route', function(done) {
+    request(app)
+      .get('/api/todos')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+
+        var id =  res.body[0]._id;
+
+        request(app)
+          .delete('/api/todos/' + id )
+          .expect('Content-Type', /json/)
+          .expect(500)
+          .end(function(err, res) {
+
+            done();
+          });
+      });
+  });
+
+  // Test the home page responds as a HTML doc
   it('returns a HTML document for / route', function(done) {
     request(app)
       .get('/')
@@ -44,6 +138,7 @@ describe('app/routes.js tests', function() {
       });
   });
 
+  // Test the app responds with 404 for unknown URLs
   it('returns 404 for a URL route that doesn\'t exist', function(done) {
     request(app)
       .get('/api/this-does-not-exist')
@@ -52,5 +147,10 @@ describe('app/routes.js tests', function() {
         if (err) throw err;
         done();
       });
+  });
+
+  // clean up test DB
+  after(function(){
+    require('mocha-mongoose')(dbURI);
   });
 });
