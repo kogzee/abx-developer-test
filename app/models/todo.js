8370f641834378288baf65a27bc2dfc3b661498c
Lee Sinclair <lee.j.sinclair@gmail.com>
2015-11-30 12:14:27 +1000

    Over engineered done, to allow muliplte todo items to be added if existing ones were complete

--- app/models/todo.js
+++ app/models/todo.js
@@ -6,14 +6,11 @@ var Todo = new Schema({
     'type': String,
     'required': 'Please enter a Todo'
   },
-  'done': { 'type': Number, 'default': -1 } /* -1 = not done */
+  'done': { 'type': Boolean } /* -1 = not done */
 });
 
 // provide the ability sort items at speed
-Todo.index({ 'text': 1 });
-
 // index by, text and done. ensure todo itemas are unique
-// this means that people can add todo items again as new items if an existing item has already been completed
-Todo.index({ 'text': 1, 'done': 1 }, { 'unique': true });
+Todo.index({ 'text': 1 }, { 'unique': true });
 
 module.exports = mongoose.model('Todo', Todo );
